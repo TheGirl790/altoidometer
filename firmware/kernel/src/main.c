cca1ad04fb0a28fd202dbd35f2037527e680ba57
TheGirl790 <hellcat7232@gmail.com>
2026-08-12 16:56:01 -0400

    added wokwi to kernel pio project, oops i mean to put this in a new branch

--- firmware/kernel/src/main.c
+++ firmware/kernel/src/main.c
@@ -1,1 +1,11 @@
-void app_main() {}
+#include "esp_partition.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    while (1) {
+        printf("hello\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
