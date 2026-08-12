dea7b4d59145e83c5af0fb11f4d12328b8def4b0
TheGirl790 <hellcat7232@gmail.com>
2026-08-12 18:12:18 -0400

    get wokwi working and implement custom partition table

--- firmware/kernel/src/main.c
+++ firmware/kernel/src/main.c
@@ -1,11 +1,35 @@
 #include "esp_partition.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
 void app_main(void)
 {
-    while (1) {
-        printf("hello\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+    esp_partition_iterator_t it =
+        esp_partition_find(
+            ESP_PARTITION_TYPE_ANY,
+            ESP_PARTITION_SUBTYPE_ANY,
+            NULL
+        );
+
+    while (it != NULL) {
+        const esp_partition_t *p = esp_partition_get(it);
+
+        printf(
+            "label: %-16s "
+            "type: 0x%02x "
+            "subtype: 0x%02x "
+            "offset: 0x%08lx "
+            "size: 0x%08lx "
+            "encrypted: %s\n",
+
+            p->label,
+            p->type,
+            p->subtype,
+            (unsigned long)p->address,
+            (unsigned long)p->size,
+            p->encrypted ? "yes" : "no"
+        );
+
+        it = esp_partition_next(it);
     }
+
+    esp_partition_iterator_release(it);
 }
