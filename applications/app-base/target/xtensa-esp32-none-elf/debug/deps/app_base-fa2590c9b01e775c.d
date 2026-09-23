2b7aa57d95acb968112c7417f5e76a165c39c5eb
TheGirl790 <hellcat7232@gmail.com>
2026-09-22 22:00:17 -0400

    Delete applications/app-base/target directory

--- applications/app-base/target/xtensa-esp32-none-elf/debug/deps/app_base-fa2590c9b01e775c.d
+++
@@ -1,5 +0,0 @@
-/home/TheGirl790/Projects/altoidometer/applications/app-base/target/xtensa-esp32-none-elf/debug/deps/app_base-fa2590c9b01e775c.d: src/main.rs
-
-/home/TheGirl790/Projects/altoidometer/applications/app-base/target/xtensa-esp32-none-elf/debug/deps/app_base-fa2590c9b01e775c: src/main.rs
-
-src/main.rs:
